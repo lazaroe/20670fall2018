1a9d4a8051470172bcd61b72934b49f5ad865ba1
Unknown <pedicho@gmail.com>
2018-08-20 14:31:59 -0600

    practice

---
+++ DGM2670/Assets/IfStatements.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+	public float Speed;
+
+	public bool LightsOn;
+	// Use this for initialization
+	void Start () {
+		{
+		//do work
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

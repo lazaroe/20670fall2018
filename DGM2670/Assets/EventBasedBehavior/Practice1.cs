368d0b2d5f975f3e918fdc36a3dd50add748f8a2
Unknown <pedicho@gmail.com>
2018-09-04 14:11:25 -0600

    practice

--- DGM2670/Assets/EventBasedBehavior/Practice1.cs
+++ DGM2670/Assets/EventBasedBehavior/Practice1.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class Practice1 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	//more notes
 }

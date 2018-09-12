7bdbd7268e25481216b26e543d20950c801da739
Unknown <pedicho@gmail.com>
2018-09-12 14:06:41 -0600

    In class

--- DGM2670/Assets/MovePattern.cs
+++ DGM2670/Assets/MovePattern.cs
@@ -14,23 +14,31 @@ public class MovePattern : ScriptableObject {
         private Vector3 moveDirection;
         private Vector3 rotDirection;
 
-		public void Invoke(CharacterController controller, Transform transform)
-		{
-		 if (controller.isGrounded)
-            {
-                moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-                //moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-                //rotatebelow
-                rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-                transform.Rotate(rotDirection);
-                moveDirection = transform.TransformDirection(moveDirection);
-                
-                if (Input.GetButton("Jump"))
-                    moveDirection.y = jumpSpeed;
+		public virtual void Invoke(CharacterController controller, Transform transform)
+    {
+        if (controller.isGrounded)
+        {
+            Move(transform);
+        }
+        Move(controller);
+    }
 
-                
-            }
-            moveDirection.y -= gravity.Value * Time.deltaTime;
-            controller.Move(moveDirection * Time.deltaTime);
-		}
+    protected void Move(CharacterController controller)
+    {
+        moveDirection.y -= gravity.Value * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    protected void Move(Transform transform)
+    {
+        moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+        //rotatebelow
+        rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+        transform.Rotate(rotDirection);
+        moveDirection = transform.TransformDirection(moveDirection);
+    }
 }
+
+
+//if (Input.GetButton("Jump"))
+                //moveDirection.y = jumpSpeed;
---
+++ DGM2670/Assets/MovePatternDual.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "MovePatternDual")]
+public class MovePatternDual : MovePattern {
+
+	public override void Invoke(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded)
+		{
+			Move(transform);
+		}
+		Move(controller);
+	}
+	
+}

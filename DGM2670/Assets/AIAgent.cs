8fc56a4fff87083ca8aee73c99a1aad0cf4094f0
Unknown <pedicho@gmail.com>
2018-09-17 14:16:44 -0600

    NavMesh

---
+++ DGM2670/Assets/AIAgent.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AIAgent : MonoBehaviour {
+
+	private NavMeshAgent agent;
+	public Transform Destination;
+	public Transform PostPoint;
+	private Transform finalDestination;
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+		finalDestination = transform;
+	}
+	private void OnTriggerEnter(Collider obj)
+	{
+		if(obj.transform == Destination)
+		finalDestination = transform;
+	}
+	private void OnTriggerExit(Collider obj)
+	{
+		finalDestination = PostPoint;
+	}
+	
+	private void Update ()
+	{
+		agent.destination = finalDestination.position;
+	
+	}
+}

1fae9aaec290d03f4a3b7d4a2cb7f550362f6160
Unknown <pedicho@gmail.com>
2018-09-19 10:09:07 -0600

    Loops

---
+++ DGM2670/Assets/Loops.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+	int cupsInTheSink = 4;
+    
+	void Start () 
+    {
+     {
+        while(cupsInTheSink > 0)
+        {
+            Debug.Log ("I've washed a cup!");
+            cupsInTheSink--;
+        }
+    }
+
+    {
+        string[] strings = new string[3];
+        
+        strings[0] = "First string";
+        strings[1] = "Second string";
+        strings[2] = "Third string";
+        
+        foreach(string item in strings)
+        {
+            print (item);
+        }
+    }
+}
+}

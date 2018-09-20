a4595978c6fa71a8511a3239ea41d6a219fa0ce9
Unknown <pedicho@gmail.com>
2018-09-20 12:24:10 -0600

    Switch Statement

---
+++ DGM2670/Assets/Scripts/Difficulty.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Difficulty : MonoBehaviour {
+
+	public int selectedLevel;
+private int easy = 0, medium = 1, hard = 2;
+void start()
+		{
+  		 switch(selectedLevel)
+		{
+		case 0:
+		print("Easy");
+		break;
+		case 1:
+		print("Medium");
+		break;
+		case 2:
+		print("Hard");
+		break;
+		default:
+		print("Not a choice");
+		break;
+		}
+	}
+}

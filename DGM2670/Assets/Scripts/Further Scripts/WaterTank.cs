7f5b308765fb38205e9caafc6f7028560b8b2268
Unknown <pedicho@gmail.com>
2018-09-20 13:24:59 -0600

    More Loops

---
+++ DGM2670/Assets/Scripts/Further Scripts/WaterTank.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTank : MonoBehaviour {
+
+	public int[] watertank = {5};
+	public int watertankcapacity = 100;
+
+	void Update ()
+	{
+		while(watertankcapacity > 1)
+		{
+			Debug.Log(watertankcapacity);
+			watertankcapacity--; 
+		}
+	}
+
+}

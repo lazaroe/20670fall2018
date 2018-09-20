6f8418589eb6b227260b46d5fc6d0610fdae66a4
Unknown <pedicho@gmail.com>
2018-09-20 12:37:12 -0600

    Start Event

---
+++ DGM2670/Assets/EventBasedBehavior/StartEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class StartEvent : MonoBehaviour {
+
+	public UnityEvent Event;
+	
+	private void Start()
+	{
+		Event.Invoke();
+	}
+}

a6c5319d32e129d6f404066c4e99cb450b00fca0
Unknown <pedicho@gmail.com>
2018-09-19 18:59:03 -0600

    Gliding

---
+++ DGM2670/Assets/Gliding.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//[CreateAssetMenu]
+//public class Gliding : ScriptableObject {
+
+	//public bool gravity;
+	
+
+	//public override void Invoke(CharacterController controller, Transform transform)
+	//{
+	//if (controller.isGrounded)
+		//{
+			//gavity(transform);
+			//doubleJump = true;
+	//	}
+//	}
+//}

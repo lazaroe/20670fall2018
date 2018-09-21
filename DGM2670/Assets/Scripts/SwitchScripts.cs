e26847cee3cd764e97fdb98905c13d128135412e
Unknown <pedicho@gmail.com>
2018-09-20 22:11:14 -0600

    Character Move Transformation

---
+++ DGM2670/Assets/Scripts/SwitchScripts.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScripts : MonoBehaviour {
+
+	public MovePattern Car;
+	public MovePattern Airplane;
+	private CharacterController controller;
+
+	private void Start ()
+	{
+		controller = GetComponent<CharacterController>();
+		Car = GetComponent<MovePattern>();
+		Airplane = GetComponent<MovePattern>();
+	}
+
+	void Update()
+	{
+
+		if (controller.isGrounded)
+		{
+			//Car.enabled = !Car.enabled;
+			Car.Invoke(controller, transform);
+		}
+		else
+		{
+			Airplane.Invoke(controller, transform);
+		}
+	}
+
+}

b7e52a8ec14b1d1d4ee7e5a4d2a23bb4719f08ac
Unknown <pedicho@gmail.com>
2018-09-26 14:25:12 -0600

    in class game

---
+++ DGM2670/Assets/GameScripts/Letter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Letter")]
+public class Letter : ScriptableObject 
+{
+	public Letters.LettersSelection Value;
+	
+}

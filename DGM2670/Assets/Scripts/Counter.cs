ad909f13fc7df62c7f1d518b48672f4c376c4619
Unknown <pedicho@gmail.com>
2018-10-08 14:02:47 -0600

    in class

---
+++ DGM2670/Assets/Scripts/Counter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+/*public class Counter : MonoBehaviour 
+{
+	public UnityEvent OnCount;
+	public float seconds = 2.0f;
+	public int numbers = 30;
+
+	IEnumerator Start ()
+	{
+		while (numbers > 0)
+		{
+		
+		}
+	}
+}
+*/

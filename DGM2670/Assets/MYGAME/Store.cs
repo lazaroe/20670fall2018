ba330614c6813ce2ffbaa9a50a6cc0613e0b58e9
Unknown <pedicho@gmail.com>
2018-10-15 14:09:35 -0600

    in class

---
+++ DGM2670/Assets/MYGAME/Store.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "store",menuName = "Store/StoreFront")]
+public class Store : MonoBehaviour {
+
+	public Objects Available;
+	public Objects Purchased;
+	public void MakePurchase(Object obj)
+	{
+		foreach (var availableObject in Available.ObjectList)
+		{
+			if (availableObject == obj)
+			{
+				Purchased.ObjectList.Add(obj);
+			}
+		}
+	}
+}

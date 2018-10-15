9649cbdf0d4e4eec1221a4a1a31c16df678ac861
Unknown <pedicho@gmail.com>
2018-10-15 14:09:53 -0600

    in class

---
+++ DGM2670/Assets/MYGAME/Objects.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objects", menuName = "Store/Objects")]
+public class Objects : ScriptableObject {
+
+	public List<Object> ObjectList;
+}

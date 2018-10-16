a306b15f8a6288e372b99de28234bf076a27a4df
Unknown <pedicho@gmail.com>
2018-10-16 16:08:24 -0600

    practice

---
+++ DGM2670/Assets/MYGAME/Build.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Build : MonoBehaviour
+{
+
+	public Transform Spawnpoint;
+	public GameObject Prefab;
+
+	void OnMouseDown()
+	{
+		Prefab = Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+		Destroy(Prefab, 2.0f);
+	}
+	
+}
---
+++ DGM2670/Assets/MYGAME/ReloadLevel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadLevel : MonoBehaviour {
+
+	void OnTriggerEnter(Collider Other)
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}

70e596e8acbebd2e74d6ee4bf5da09d546b07126
Unknown <pedicho@gmail.com>
2018-10-22 16:33:01 -0600

    In class

--- DGM2670/Assets/MYGAME/Build.cs
+++ DGM2670/Assets/MYGAME/Build.cs
@@ -7,11 +7,12 @@ public class Build : MonoBehaviour
 
 	public Transform Spawnpoint;
 	public GameObject Prefab;
-
-	void OnMouseDown()
-	{
+	
+	//public virtual void Invoke(CharacterController controller, Transform transform)
+	 	void OnMouseDown()
+			{
 		Prefab = Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
 		Destroy(Prefab, 2.0f);
-	}
+			}
 	
 }

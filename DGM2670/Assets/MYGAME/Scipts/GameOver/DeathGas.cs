13addfaf8822aad6c2c79a8fce11304e7864c43e
Unknown <pedicho@gmail.com>
2018-10-27 18:43:48 -0600

    progress

---
+++ DGM2670/Assets/MYGAME/Scipts/GameOver/DeathGas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathGas : MonoBehaviour {
+
+public void update ()
+{   
+		transform.Translate(0f,1f*Time.deltaTime,0f);
+}
+	void OnCollisionEnter2D	(Collision2D col)
+{
+	if (gameObject.CompareTag("Player"))
+    {
+        gameObject.SetActive(false);
+    }
+}
+}

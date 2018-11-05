891fd9e9513ee76064641bff273f8f35eb7d5fe9
Unknown <pedicho@gmail.com>
2018-11-05 14:31:28 -0700

    in class

---
+++ DGM2670/Assets/MYGAME/Scipts/JetPack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPack : MonoBehaviour {
+
+	public float jumpForce = 10f;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.relativeVelocity.y <= 0f)
+		{
+			Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+			if (rb != null)
+			{
+				Vector3 velocity = rb.velocity;
+				velocity.y = jumpForce;
+				rb.velocity = velocity;
+			}     
+		}
+		
+
+
+
+	}
+
+}

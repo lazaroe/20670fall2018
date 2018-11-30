a8cf01954f45c27361b78b6d3f74ee3b39de32e2
Unknown <pedicho@gmail.com>
2018-11-29 17:28:19 -0700

    practice

---
+++ DGM2670/Assets/MYGAME/Scipts/Shop/Store Advanced/UpdateText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateText : MonoBehaviour {
+
+	public IntData Data;
+	private Text label;
+
+	void Start ()
+	{
+		label = GetComponent<Text>();
+		OnUpdate();
+	}
+
+	public void OnUpdate()
+	{
+		label.text = Data.Value.ToString("C0");
+	}
+}

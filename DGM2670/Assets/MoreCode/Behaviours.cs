f46f44609172c301e284b2024461141a84342d52
Unknown <pedicho@gmail.com>
2019-02-01 12:40:27 -0700

    daily commit

---
+++ DGM2670/Assets/MoreCode/Behaviours.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Behaviours : MonoBehaviour {
+	
+	public UnityEventQueueSystem OnAwake, OnStart, TriggerEnter, CollisionEnter;
+
+	private void Awake()
+	{
+		OnAwake.Invoke;
+	}
+
+	void Start ()
+	{
+		OnStart.Invoke;
+	}
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		TriggerEnter.Invoke;
+	}
+
+	private void OnCollisionEnter (Collision other)
+	{
+		CollisionEnter.Invoke;
+	}
+
+	private void Ondestroy ()
+	{
+		
+	}
+
+}
